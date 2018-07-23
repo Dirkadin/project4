6a76bfd148a65ef549bd40b7dddd270559f4b7cd
dirk <dirkadin@gmail.com>
2018-07-23 09:37:12 -0500

    removed unneeded init added second menu

--- src/NavManager.java
+++ src/NavManager.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class NavManager {
@@ -7,12 +8,9 @@ public class NavManager {
     private Scanner scanner = null;
     private Scanner kbScanner = null;
 
-    private NavManager() {
-
-
-
-    }
-
+    /**
+     * Menu for the top level of the program
+     */
     public void startProgram() {
 
         System.out.println("Please select an option: ");
@@ -28,16 +26,47 @@ public class NavManager {
                 break;
             case 3: break;
             case 4:
+                if (scanner != null) {
+                    scanner.close();
+                }
                 System.exit(0);
                 break;
             default:
             System.out.println("Please enter an Integer 1 through 4!");
             NavManager.shared.startProgram();
         }
-
-
     }
 
+    public void manipulationMenu(String fileName) {
+        System.out.println("Please select an option:");
+        System.out.println("1. Swap red and blue color values");
+        System.out.println("2. Convert to grayscale");
+        System.out.println("3. Flip the image(s)");
+        System.out.println("4. Flop the image(s)");
+        System.out.println("5. Invert the image(s)");
+        System.out.println("6. Return to the main menu");
+        System.out.println("7. Exit the program");
+
+        switch (getKBScanner().nextInt()) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+            case 6:
+                NavManager.shared.startProgram();
+                break;
+            case 7:
+                if (scanner != null) {
+                    scanner.close();
+                }
+                System.exit(0);
+        }
+    }
+
+    /**
+     * @return - Returns the Scanner instance used for getting keyboard input inside of the NavManager
+     */
     private Scanner getKBScanner() {
         if (kbScanner != null) {
             return kbScanner;
@@ -48,9 +77,12 @@ public class NavManager {
         return localScanner;
     }
 
+    /**
+     * Gets a user specified filename and calls the manipulation menu
+     */
     public void readFile() {
-        System.out.println("Please enter a filename");
+        System.out.println("Please enter a filename (without the extension): ");
         String fileName = getKBScanner().nextLine() + ".ppm";
-        System.out.println(fileName);
+        NavManager.shared.manipulationMenu(fileName);
     }
 }
